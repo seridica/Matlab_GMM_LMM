adb8d7491ab1e0fe6d48857c272b0198cd7d917b
seridica <calvink@stanford.edu>
2019-07-04 14:48:59 -0700

    Updated to plot the 95% probability ellipse

--- MUdataJacques.m
+++ MUdataJacques.m
@@ -1,7 +1,7 @@
 clear all
 close all
 
-filename = ['D:\UBC - Postdoc\Sensorimotor\Jacques Gaussian Mixture\trunk\MU_distribution_subject.xlsx'];
+filename = ['D:\UBC - Postdoc\EMG Related\Jacques Gaussian Mixture\Code\trunk\MU_distribution_subject.xlsx'];
 %data = xlsread(filename,2,'B1:C74');
 dataFull = xlsread(filename,1,'B1:D74');
 
@@ -40,7 +40,7 @@ dataSubject = [dataSubject, subjectData];
 
 GMModels = cell(2,1); 
 options = statset('MaxIter',100000,'TolX', 1.0000e-09);
-for j = 1:3
+for j = 1:2
     if j==3
         rng('shuffle');
         GMModels{j} = fitgmdist_lmm(dataSubject,j, 'Start', 'randSample','options',options);
@@ -54,7 +54,53 @@ for j = 1:3
     Mu = GMModels{j}.mu
     Sigma = GMModels{j}.Sigma
     AIC = GMModels{j}.AIC
-    bhi = GMModels{j}.bhi
-    Sigma_eff = GMModels{j}.Sigma_eff
+%     bhi = GMModels{j}.bhi
+%     Sigma_eff = GMModels{j}.Sigma_eff
 end
 
+figure;
+figInd = [1 3];
+for j = 1:2
+    subplot(2,2,figInd(j))
+    h1 = surf(numM);
+    view(0,90);
+    shading interp;
+    caxis([0 30]);
+    colorbar('off');
+    h = gca;
+    hold on
+    ylim([1 12]);
+    xlim([1 7]);
+    title('Data');
+    ylabel('Centroid location');
+    xlabel('Distribution');
+    subplot(2,2,figInd(j)+1)
+    %ezcontour(@(x1,x2)pdf(GMModels{j},[x2 x1]),[1 7 1 12])
+    for k=1:j
+        hold on;
+        
+        % Chi-Square value for 95%
+        s = chi2inv(0.95, j);
+        
+        % Eigenvalue / Eigenvector for variance (defines elipse)
+        [V, D] = eig( GMModels{j}.Sigma(:,:,k) * s);
+
+        t = linspace(0, 2 * pi);
+        a = (V * sqrt(D)) * [cos(t(:))'; sin(t(:))'];
+
+        plot(a(2, :) + GMModels{j}.mu(k,2), a(1, :) + GMModels{j}.mu(k,1), 'b');
+        plot( GMModels{j}.mu(k,2), GMModels{j}.mu(k,1), 'r+' );
+    end
+    xlim([0,6])
+    ylim([0,12])
+%     
+%     for i=1:8
+%         for k=1:j
+%             plot( GMModels{j}.bhi(k,2,i) + GMModels{j}.mu(k,2), ...
+%                   GMModels{j}.bhi(k,1,i) + GMModels{j}.mu(k,1), 'kx' );
+%         end
+%     end
+    title(sprintf('GM Model - %i Component(s)',j));
+    ylabel('Centroid location');
+    xlabel('Distribution');
+end
